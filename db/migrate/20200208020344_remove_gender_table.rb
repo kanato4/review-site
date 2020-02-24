d7d9e93fe5d5cfa806b1bec7a94dfc7adcf19db2
kanato4 <kntykt@icloud.com>
2020-02-24 23:53:05 +0900

    change DB
    migrateを修正。

--- db/migrate/20200208020344_remove_gender_table.rb
+++
@@ -1,5 +0,0 @@
-class RemoveGenderTable < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :genders
-  end
-end
--- db/migrate/20200219120527_drop_table_spots.rb
+++
@@ -1,5 +0,0 @@
-class DropTableSpots < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :spots
-  end
-end
